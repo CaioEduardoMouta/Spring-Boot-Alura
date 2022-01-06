bc6f3c1d8386eb8d347da756879ccdff22eee32e
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-01-05 23:22:51 -0300

    JDBC

---
+++ loja-virtual/src/main/java/TestaInsercaoEListagemComProduto.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+
+import java.sql.SQLException;
+
+
+import modelo.Produto;
+
+public class TestaInsercaoEListagemComProduto {
+	
+	public static void main(String[] args) throws SQLException {
+		
+		Produto comoda = new Produto("Cômoda","Cômoda Vertical");
+		
+		try(Connection connection = new ConnectionFactory().recuperarConexao()) {
+			ProdutoDAO produtoDAO = new ProdutoDAO(connection);
+			produtoDAO.salvarProduto(comoda);
+		}
+	}
+
+}
