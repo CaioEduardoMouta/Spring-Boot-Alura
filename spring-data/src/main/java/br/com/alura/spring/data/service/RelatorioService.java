8b093726fe605cc60b993e5256a793ddd8bdd2aa
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-01-17 23:07:04 -0300

    spring-data

--- spring-data/src/main/java/br/com/alura/spring/data/service/RelatorioService.java
+++ spring-data/src/main/java/br/com/alura/spring/data/service/RelatorioService.java
@@ -32,6 +32,8 @@ public class RelatorioService {
 			System.out.println("Qual acao de cargo deseja executar");
 			System.out.println("0 - Sair");
 			System.out.println("1 - Busca funcionario nome");
+			System.out.println("2 - Busca funcionario nome, data contratacao e salario maior");
+			System.out.println("3 - Busca funcionario data contratacao");
 			
 			
 			int action = scanner.nextInt();
@@ -42,6 +44,9 @@ public class RelatorioService {
 				break;
 			case 2:
 				buscaFuncionarioNomeSalarioMaiorData(scanner);
+				break;
+			case 3:
+				buscaFuncionarioDataContratacao(scanner);
 				break;
 			default:
 				system = false;
@@ -72,6 +77,16 @@ public class RelatorioService {
 		
 		List<Funcionario> list = funcionarioRepository
 				.findNomeSalarioMaiorDataContratacao(nome, salario, localDate);
+		list.forEach(System.out::println);
+	}
+	
+	private void buscaFuncionarioDataContratacao(Scanner scanner) {
+		System.out.println("Qual data contratacao deseja pesquisar");
+		String data = scanner.next();
+		LocalDate localDate = LocalDate.parse(data, formatter);
+		
+		List<Funcionario> list = funcionarioRepository.findDataContratacaoMaior(localDate);
+		list.forEach(System.out::println);
 	}
 }
 
