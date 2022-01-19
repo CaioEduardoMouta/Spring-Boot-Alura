b994647a47c45c57073e67905dab194712b55390
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-01-18 23:27:13 -0300

    SpringData

--- spring-data/src/main/java/br/com/alura/spring/data/service/RelatorioService.java
+++ spring-data/src/main/java/br/com/alura/spring/data/service/RelatorioService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.alura.spring.data.orm.Cargo;
 import br.com.alura.spring.data.orm.Funcionario;
+import br.com.alura.spring.data.orm.FuncionarioProjecao;
 import br.com.alura.spring.data.repository.CargoRepository;
 import br.com.alura.spring.data.repository.FuncionarioRepository;
 
@@ -34,6 +35,8 @@ public class RelatorioService {
 			System.out.println("1 - Busca funcionario nome");
 			System.out.println("2 - Busca funcionario nome, data contratacao e salario maior");
 			System.out.println("3 - Busca funcionario data contratacao");
+			System.out.println("4 - Pesquisa funcionario salario");
+
 			
 			
 			int action = scanner.nextInt();
@@ -47,6 +50,9 @@ public class RelatorioService {
 				break;
 			case 3:
 				buscaFuncionarioDataContratacao(scanner);
+				break;
+			case 4:
+				pesquisafuncionarioSalario();
 				break;
 			default:
 				system = false;
@@ -87,6 +93,12 @@ public class RelatorioService {
 		
 		List<Funcionario> list = funcionarioRepository.findDataContratacaoMaior(localDate);
 		list.forEach(System.out::println);
+	}
+	
+	private void pesquisafuncionarioSalario() {
+		List<FuncionarioProjecao> list = funcionarioRepository.findFuncionarioSalario();
+		list.forEach(f -> System.out.println("Funcionario: id " + f.getId()
+		+ " | nome" + f.getNome() + " | salario" + f.getSalario()));
 	}
 }
 
