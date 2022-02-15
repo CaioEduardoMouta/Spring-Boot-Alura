4c87fde2d022cf2713c0ee4866f8370825434f73
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-02-14 23:47:50 -0300

    Spring

--- forum/src/test/java/br/com/alura/forum/ForumApplicationTests.java
+++ forum/src/test/java/br/com/alura/forum/ForumApplicationTests.java
@@ -1,13 +1,18 @@
 package br.com.alura.forum;
 
 
+import org.junit.Assert;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class ForumApplicationTests {
 
 	@org.junit.Test
-	void contextLoads() {
+	public void contextLoads() {
+		Assert.assertTrue(true);
 	}
 
 }
