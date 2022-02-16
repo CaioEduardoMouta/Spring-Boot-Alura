1d2e3c0c697829ace115d956dd8c39240137cc28
CAIO MOUTA <caioeduardo1993@gmail.com>
2022-02-15 23:55:48 -0300

    Spring

--- forum/src/main/java/br/com/alura/forum/controller/AutenticaoController.java
+++ forum/src/main/java/br/com/alura/forum/controller/AutenticaoController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -20,6 +21,7 @@ import br.com.alura.forum.controller.form.LoginForm;
 
 @RestController
 @RequestMapping("/auth")
+@Profile(value = {"prod", "test"})
 public class AutenticaoController {
 	
 	@Autowired
